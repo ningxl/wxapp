1fbc7d5ff1b18450fb0c6640294a1905d6e3f430
nxl <1033211644@qq.com>
2019-12-01 21:03:33 +0800

    add cart

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    listData: ['热销推荐', '鲜果鲜茶', '益菌多多', '法式奶霜','香浓牛奶'],
+    listData: ['热销推荐', '鲜果鲜茶', '益菌多多', '法式奶霜', '香浓牛奶'],
     imgUrls: [
       '../../images/1.png',
       '../../images/3.png',
@@ -14,31 +14,105 @@ Page({
     autoplay: true,
     interval: 5000,
     duration: 1000,
-    goodsData:[
-      {url: '../../images/goodsimg.png', name : '17岁轻茶奶霜（大杯）', price : 14.00},
-      {url: '../../images/goodsimg.png', name : '鲜百香双响炮（大杯）', price : 13.00},
-      {url: '../../images/goodsimg.png', name : '17岁轻茶奶霜（大杯）', price : 12.00},
-      {url: '../../images/goodsimg.png', name : '鲜百香双响炮（大杯）', price : 11.00},
-      {url: '../../images/goodsimg.png', name : '17岁轻茶奶霜（大杯）', price : 10.50},
-      {url: '../../images/goodsimg.png', name : '鲜百香双响炮（大杯）', price : 8.00},
-      {url: '../../images/goodsimg.png', name : '17岁轻茶奶霜（大杯）', price : 14.00},
-      {url: '../../images/goodsimg.png', name : '鲜百香双响炮（大杯）', price : 13.00},
-      {url: '../../images/goodsimg.png', name : '17岁轻茶奶霜（大杯）', price : 12.00},
-      {url: '../../images/goodsimg.png', name : '鲜百香双响炮（大杯）', price : 11.00},
-      {url: '../../images/goodsimg.png', name : '17岁轻茶奶霜（大杯）', price : 10.50},
-      {url: '../../images/goodsimg.png', name : '鲜百香双响炮（大杯）', price : 8.00},
+    goodsData: [
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 14.00 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 13.00 },
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 12.00 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 11.00 },
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 10.50 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 8.00 },
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 14.00 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 13.00 },
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 12.00 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 11.00 },
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 10.50 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 8.00 },
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 14.00 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 13.00 },
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 12.00 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 11.00 },
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 10.50 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 8.00 },
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 14.00 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 13.00 },
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 12.00 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 11.00 },
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 10.50 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 8.00 },
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 14.00 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 13.00 },
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 12.00 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 11.00 },
+      { url: '../../images/goodsimg.png', name: '17岁轻茶奶霜（大杯）', price: 10.50 },
+      { url: '../../images/goodsimg.png', name: '鲜百香双响炮（大杯）', price: 8.00 },
     ],
-    cartPrice: 0
+    listSelect: 0,
+    showCart: false,
+    cartData: [],
+    cartPrice: 0,
   },
-  addToCart:function(event){
-    this.setData({cartPrice: this.data.cartPrice+this.data.goodsData[event.currentTarget.dataset.index].price});
+  changeCart: function(){
+    if(this.data.cartPrice != 0)
+    this.setData({
+      showCart: !this.data.showCart
+    })
   },
-  goToPay:function(){
-    if(this.data.cartPrice != 0){
+  addToCart: function (event) {
+    this.setData({
+      cartData: this.data.cartData.concat([{name: this.data.goodsData[event.currentTarget.dataset.index].name, price: this.data.goodsData[event.currentTarget.dataset.index].price}])
+    })
+    this.setData({
+      cartPrice: this.data.cartPrice + this.data.goodsData[event.currentTarget.dataset.index].price
+    });
+  },
+  removeItemFromCart: function(event){
+    this.setData({
+      cartPrice: this.data.cartPrice - this.data.cartData[event.currentTarget.dataset.index].price
+    });
+    var t = this.data.cartData;
+    t.splice(event.currentTarget.dataset.index,1)
+    this.setData({
+      cartData: t
+    })
+    if(this.data.cartPrice == 0)
+    this.setData({
+      showCart: false
+    })
+  },
+  goToPay: function () {
+    if (this.data.cartPrice != 0) {
       wx.setStorageSync('cartPrice', this.data.cartPrice);
       wx.navigateTo({
         url: '../payment/payment'
       })
     }
-  }
+  },
+  scroll: function (e) {
+    var dis = e.detail.scrollTop
+    if (dis > 0 && dis < 300) {
+      this.setData({
+        listSelect: 0,
+      })
+    }
+    if (dis > 300 && dis < 600) {
+      this.setData({
+        listSelect: 1,
+      })
+    }
+    if (dis > 600 && dis < 900) {
+      this.setData({
+        listSelect: 2,
+      })
+    }
+    if (dis > 900 && dis < 1200) {
+      this.setData({
+        listSelect: 3,
+      })
+    }
+    if (dis > 1200) {
+      this.setData({
+        listSelect: 4,
+      })
+    }
+  },
 })
